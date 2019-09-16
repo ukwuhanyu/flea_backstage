ad2c5c28627c2499c85819d682a35c6856a5b9e0
wuhanyu <1134744136@qq.com>
2019-09-16 12:31:42 +0800

    axios初始化

---
+++ src/common/axios-init.js
@@ -0,0 +1,29 @@
+import axios from 'axios'
+import { Loading } from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
+
+// 接口默认请求地址 （在dev.env.js里面配置）
+axios.defaults.baseURL = process.env.BASE_URL
+
+// 解决axios发post请求
+axios.defaults.headers.post['Content-Type'] = 'application/json;charset-UTF-8'
+
+// 添加请求拦截器
+axios.interceptors.request.use(function (config) {
+  // 在发送请求之前做些什么
+  return config;
+}, function (error) {
+  // 对请求错误做些什么
+  return Promise.reject(error);
+});
+
+// 添加响应拦截器
+axios.interceptors.response.use(function (response) {
+  // 对响应数据做点什么
+  return response;
+}, function (error) {
+  // 对响应错误做点什么
+  return Promise.reject(error);
+});
+
+export default axios
