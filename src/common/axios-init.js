8b2fd6292a99d5f4ef13619ec86f9047d2893671
wuhanyu <1134744136@qq.com>
2019-09-18 18:43:10 +0800

    http封装

--- src/common/axios-init.js
+++ src/common/axios-init.js
@@ -27,37 +27,5 @@ axios.interceptors.response.use(function (response) {
   return Promise.reject(error);
 });
 
-/**
- * get方法，对应get请求
- * @param {String} url [请求的url地址]
- * @param {Object} params [请求时携带的参数]
- */
-export function get(url, params){    
-  return new Promise((resolve, reject) =>{        
-      axios.get(url, {            
-          params: params        
-      }).then(res => {
-          resolve(res.data);
-      }).catch(err =>{
-          reject(err.data)        
-  })    
-});}
-
-/** 
- * post方法，对应post请求 
- * @param {String} url [请求的url地址] 
- * @param {Object} params [请求时携带的参数] 
- */
-export function post(url, params) {
-  return new Promise((resolve, reject) => {
-      axios.post(url, QS.stringify(params))
-      .then(res => {
-          resolve(res.data);
-      })
-      .catch(err =>{
-          reject(err.data)
-      })
-  });
-}
 
 export default axios
