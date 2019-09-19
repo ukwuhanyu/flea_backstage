a6bf54921fe43501253fe3ab86fcf1594778fb78
wuhanyu <1134744136@qq.com>
2019-09-19 22:31:50 +0800

    menu01

--- src/components/Index/Index.vue
+++ src/components/Index/Index.vue
@@ -40,36 +40,43 @@
               active-text-color="#ffd04b"
               :collapse="isCollapse"
               :collapse-transition="transition">
-              <el-submenu index="1">
+              <el-submenu :index="(index+1).toString()" v-for="(item, index) in menuList" :key="index">
                 <template slot="title">
                   <i class="el-icon-location"></i>
-                  <span slot="title">导航一</span>
+                  <span slot="title">{{item.menuName}}</span>
                 </template>
-                <el-menu-item-group>
-                  <span slot="title">分组一</span>
-                  <el-menu-item index="1-1">选项1</el-menu-item>
-                  <el-menu-item index="1-2">选项2</el-menu-item>
-                </el-menu-item-group>
-                <el-menu-item-group title="分组2">
-                  <el-menu-item index="1-3">选项3</el-menu-item>
-                </el-menu-item-group>
-                <el-submenu index="1-4">
-                  <span slot="title">选项4</span>
-                  <el-menu-item index="1-4-1">选项1</el-menu-item>
+                <!-- <el-menu-item-group> -->
+                  <!-- <span slot="title">分组一</span> -->
+                  <!-- <el-menu-item index="1-1">选项1</el-menu-item> -->
+                  <!-- <el-menu-item index="1-2">
+                    <i class="el-icon-location"></i>
+                    <span slot="title">XXX</span>
+                  </el-menu-item> -->
+                <!-- </el-menu-item-group> -->
+                <!-- <el-menu-item-group title="分组2"> -->
+                  <!-- <el-menu-item index="1-3">选项3</el-menu-item> -->
+                <!-- </el-menu-item-group> -->
+                <el-submenu :index="(index+1).toString()+'-'+(index2+1).toString()" v-for="(item2, index2) in item.twoSubmenus" :key="index2">
+                  <span slot="title">{{item2.menuName}}</span>
+                  <el-menu-item :index="(index+1).toString()+'-'+(index2+1).toString()+'-'+(index3+1).toString()" v-for="(item3, index3) in item2.threeSubmenus" :key="index3">{{item.menuName}}</el-menu-item>
                 </el-submenu>
               </el-submenu>
-              <el-menu-item index="2">
+
+              <!-- <el-menu-item index="2">
                 <i class="el-icon-menu"></i>
                 <span slot="title">导航二</span>
               </el-menu-item>
+
               <el-menu-item index="3" disabled>
                 <i class="el-icon-document"></i>
                 <span slot="title">导航三</span>
               </el-menu-item>
+
               <el-menu-item index="4">
                 <i class="el-icon-setting"></i>
                 <span slot="title">导航四</span>
-              </el-menu-item>
+              </el-menu-item> -->
+
             </el-menu>
           </el-col>
         </div>
@@ -88,7 +95,8 @@ export default {
   data () {
     return {
       isCollapse: true,
-      transition: true
+      transition: true,
+      menuList: '' // 菜单列表
     }
   },
   methods: {
@@ -99,6 +107,7 @@ export default {
   created () {
     getMenu().then(res => {
       console.log(res,9999)
+      this.menuList = res
     })
   }
 }
