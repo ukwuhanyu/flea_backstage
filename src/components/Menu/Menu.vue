0720aa7278efbfaad69c87832d9e9bce15786463
wuhanyu <1134744136@qq.com>
2019-09-28 22:00:57 +0800

    menu change

--- src/components/Menu/Menu.vue
+++ src/components/Menu/Menu.vue
@@ -2,33 +2,33 @@
   <div class="menu-content">
     <div class="m_operation">
       <el-button type="primary" class="el-icon-plus">新增</el-button>
-      <el-button type="primary" class="el-icon-edit">修改</el-button>      
     </div>
     <el-container>
       <el-row>
         <el-col :span="14">
           <div class="menu_left">
             <el-table
-              :data="tableData1"
+              :data="menuList"
               style="width: 100%"
               row-key="id"
               border
-              lazy
-              :load="load"
               :tree-props="{children: 'children', hasChildren: 'hasChildren'}">
               <el-table-column
-                prop="date"
+                prop="menuName"
                 label="菜单名称"
                 width="300">
               </el-table-column>
               <el-table-column
-                prop="name"
+                prop="menuUrl"
                 label="请求地址"
-                width="180">
+                width="100">
               </el-table-column>
               <el-table-column
-                prop="address"
                 label="操作">
+                <template slot-scope="scope">
+                  <el-button type="primary" plain size="small">修改</el-button>
+                  <el-button type="danger" plain size="small" @click="del(scope.row.menuId)">删除</el-button>
+                </template>
               </el-table-column>
             </el-table>
           </div>
@@ -38,10 +38,12 @@
   </div>
 </template>
 <script>
+import {getMenu} from './model'
 export default {
   name: "Menu",
   data() {
     return {
+      menuList: [],
       form: {
         name: ''
       },
@@ -60,7 +62,23 @@ export default {
         date: '2016-05-01',
         name: '王小虎',
         address: '上海市普陀区金沙江路 1519 弄',
-        hasChildren: true
+        children: [{
+            id: 31,
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄',
+            children: [{
+              id: 311,
+              date: '2016-05-01',
+              name: '王小虎3333',
+              address: '上海市普陀区金沙江路 1519 弄',
+            }]
+          }, {
+            id: 32,
+            date: '2016-05-01',
+            name: '王小虎',
+            address: '上海市普陀区金沙江路 1519 弄'
+        }]
       }, {
         id: 4,
         date: '2016-05-03',
@@ -70,23 +88,38 @@ export default {
     };
   },
   methods: {
-    load(tree, treeNode, resolve) {
-      setTimeout(() => {
-        resolve([
-          {
-            id: 31,
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
-          }, {
-            id: 32,
-            date: '2016-05-01',
-            name: '王小虎',
-            address: '上海市普陀区金沙江路 1519 弄'
+    getMenuList () {
+      getMenu().then(res => {
+        res.data.forEach((item, index) => {
+          item.id = index + 1
+          console.log(item,2222)
+          if(item.twoSubmenus.length != 0){
+            item.children = item.twoSubmenus
+            item.children.forEach((item2, index2) => {
+              item2.id = index2 + 1
+              if(item2.threeSubmenus.length != 0) {
+                item2.children = item.threeSubmenus
+                // item2.children.forEach((item3, index3) => {
+                //   item3.id = index3 + 1
+                // })
+                console.log(item2,7777)
+              }
+              // item2.children.forEach((item3, index3) => {
+              //   item3.id = index3 + 1
+              // })
+            }) 
           }
-        ])
-      }, 1000)
-    }    
+ 
+        })
+        console.log(res.data)
+        this.menuList = res.data
+
+        console.log(this.menuList,'menu')
+      })
+    }
+  },
+  created () {
+    this.getMenuList()
   }
 };
 </script>
--- src/components/Menu/model.js
+++ src/components/Menu/model.js
@@ -3,4 +3,4 @@
  */
 import { get, post } from '../../common/axios-init'
 // 获取菜单
-export const getMenu = () => get('/back-menu')
+export const getMenu = () => get('/back-menu/')
